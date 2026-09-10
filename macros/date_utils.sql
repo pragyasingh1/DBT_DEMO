91f14f4e303fa932b36abeb499dd86bf9a6304ea
Pragya Asawat <singhpragya479@gmail.com>
2026-09-10 07:59:19 +0000

    Macros

---
+++ macros/date_utils.sql
@@ -0,0 +1,21 @@
+{% macro get_season(x) %}
+
+CASE WHEN MONTH(TO_TIMESTAMP({{x}})) in (4,5,6)
+THEN 'SUMMER'
+WHEN MONTH(TO_TIMESTAMP({{x}}))  in (10,11,12,1)
+THEN 'WINTER'
+WHEN MONTH(TO_TIMESTAMP({{x}}))  in (7,8,9)
+THEN 'RAINY'
+ELSE 'SPRING'
+END
+
+{% endmacro %}
+
+{{% macro day_type(x) %}}
+CASE
+WHEN DAYNAME(TO_TIMESTAMP({{x}})) in ('Sat','Sun')
+THEN 'Weekend'
+ELSE 'Businessday'
+END
+
+{{% endmacro %}}
--- models/example/date_dimension.sql
+++ models/example/date_dimension.sql
@@ -3,20 +3,9 @@ select TO_TIMESTAMP(time) as TIME,
 DATE(TO_TIMESTAMP(time)) as DATE,
 HOUR(TO_TIMESTAMP(time)) as HOUR_TIME,
 
-CASE
-WHEN DAYNAME(TO_TIMESTAMP(time)) in ('Sat','Sun')
-THEN 'Weekend'
-ELSE 'Businessday'
-END AS DAY_TYPE ,
+{{day_type(time)}} AS DAY_TYPE ,
 
-CASE WHEN MONTH(TO_TIMESTAMP(time)) in (4,5,6)
-THEN 'SUMMER'
-WHEN MONTH(TO_TIMESTAMP(time)) in (10,11,12,1)
-THEN 'WINTER'
-WHEN MONTH(TO_TIMESTAMP(time)) in (7,8,9)
-THEN 'RAINY'
-ELSE 'SPRING'
-END AS Station_Type
+{{get_season(time)}} AS Station_Type
 
  from {{ source('demo_source', 'WEATHER') }}
 
